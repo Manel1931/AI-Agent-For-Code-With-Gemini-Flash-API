eff48b1d2ef5575fc11ea24db570212156133e7f
Manel1931 <manelboumadane01@gmail.com>
2025-11-04 08:58:27 +0100

    Adding a list of prompts

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
 
+    if len(sys.argv) < 2:
+        print("i need a prompt !")
+        sys.exit(1)
+    prompt = sys.argv[1]
+    
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+]
+    
+    print("Args", sys.argv)
+
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        model='gemini-2.5-flash',
+        contents=messages,
     )
     print(response.text)
     if response is None or response.usage_metadata is None:
