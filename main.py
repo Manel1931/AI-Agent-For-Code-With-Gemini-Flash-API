a5b3acdd66f0b81771b9d9dd8d42dd53b1d28a5b
Manel1931 <manelboumadane01@gmail.com>
2025-11-04 07:33:45 +0100

    Add initial AI-Agent code files

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+    print(response.text)
+    if response is None or response.usage_metadata is None:
+        print("response is malformed")
+        return
+    print(f"prompt_tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"response_tokens: {response.usage_metadata.candidates_token_count}")
+
+main()
